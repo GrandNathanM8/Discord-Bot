013851f56b1ed6da30e10614cd519c71a3e3f62c
GrandNathanM8 <133559701+GrandNathanM8@users.noreply.github.com>
2023-06-06 15:51:44 +0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is ready. Logged in as {bot.user.name}')
+
+@bot.event
+async def on_message(message):
+    if message.author.id == 1111444119680733195:
+        thunder_emoji = '\N{THUNDER CLOUD AND RAIN}'
+        await message.add_reaction(thunder_emoji)
+
+    await bot.process_commands(message)
+
+bot.run('MTExNTUzMDU5ODM4NDE1MjY0Ng.G_8XuI.4amiaxf7GIQvw1Pjj2lhw7R8RK248xA8qWmRsc')
